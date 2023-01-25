323162ea738459fe061ae5025fd5e9432144d3be
Brandon Watson <bmwtsn99@gmail.com>
2023-01-24 17:28:26 -0700

    updated fuzz function

--- mayhem/fuzzCore.go
+++ mayhem/fuzzCore.go
@@ -1,62 +1,61 @@
 package fuzzCore
 
 import "strconv"
-// import "unsafe"
+import "unsafe"
 import "github.com/dicedb/dice/core"
 import "github.com/dicedb/dice/core/dencoding"
+import "github.com/dicedb/dice/testutils"
 
 func mayhemit(bytes []byte) int {
 
     var num int
-    if len(bytes) < 1 {
-        num = 0
-    } else {
+    if len(bytes) > 2 {
         num, _ = strconv.Atoi(string(bytes[0]))
+        bytes = bytes[1:]
+
+        switch num {
+
+        case 0:
+            dencoding.DecodeUInt(bytes)
+            return 0
+
+        case 1:
+            content := string(bytes)
+            core.Get(content)
+            return 0
+
+        case 2:
+            content := string(bytes)
+            core.Del(content)
+            return 0
+
+        case 3:
+            content := string(bytes)
+            var pointer = unsafe.Pointer(&content)
+            core.DelByPtr(pointer)
+            return 0
+
+        case 4:
+            content := string(bytes)
+            var test core.StackRef
+            test.Push(content)
+            return 0
+
+        default:
+            split := num
+            if split >= len(bytes) {
+
+                split = len(bytes) - 1
+            }
+
+            bytes1 := bytes[0:split]
+            bytes2 := bytes[split:len(bytes)]
+
+            testutils.EqualByteSlice(bytes1, bytes2)
+            return 0
+        }
     }
-
-    switch num {
-    
-    case 0:
-        var test core.Client
-        test.Write(bytes)
-        return 0
-
-    // this test case seemes to cause a mayhem error
-    // where the run hangs on 5 tests with 1 defect for the whole time
-    // without it the works fine getting plenty of test cases
-    
-    // case 1:
-    //     content := string(bytes)
-    //     var pointer = unsafe.Pointer(&content)
-    //     var value = uint32(num)
-    //     var test core.EvictionPool
-    //     test.Push(pointer, value)
-    //     return 0
-
-    case 2:
-        var n = uint64(num)
-        dencoding.EncodeUInt(n)
-        return 0
-
-    case 3:
-        dencoding.DecodeUInt(bytes)
-        return 0
-
-    case 4:
-        content := string(bytes)
-        core.Get(content)
-        return 0
-
-    case 5:
-        var test core.Client
-        test.Read(bytes)
-        return 0
-
-    default:
-        content := string(bytes)
-        core.Del(content)
-        return 0
-    }
+    return 0
 }
 
 func Fuzz(data []byte) int {
