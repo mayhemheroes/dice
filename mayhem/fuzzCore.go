275c4effaa1f1116e7acd2168ba7d0d59681fed6
Brandon Watson <bmwtsn99@gmail.com>
2023-01-24 21:34:04 -0700

    updated fuzz functions

--- mayhem/fuzzCore.go
+++ mayhem/fuzzCore.go
@@ -5,6 +5,7 @@ import "unsafe"
 import "github.com/dicedb/dice/core"
 import "github.com/dicedb/dice/core/dencoding"
 import "github.com/dicedb/dice/testutils"
+import fuzz "github.com/AdaLogics/go-fuzz-headers"
 
 func mayhemit(bytes []byte) int {
 
@@ -19,24 +20,58 @@ func mayhemit(bytes []byte) int {
             dencoding.DecodeUInt(bytes)
             return 0
 
+        case 5:
+            fuzzConsumer := fuzz.NewConsumer(bytes)
+            var test uint64
+            err := fuzzConsumer.CreateSlice(&test)
+            if err != nil {
+                return 0
+            }
+            dencoding.EncodeUInt(test)
+            return 0
+
         case 1:
-            content := string(bytes)
+            fuzzConsumer := fuzz.NewConsumer(bytes)
+            var content string
+            err := fuzzConsumer.CreateSlice(&content)
+            if err != nil {
+                return 0
+            }
+
             core.Get(content)
             return 0
 
         case 2:
-            content := string(bytes)
+            fuzzConsumer := fuzz.NewConsumer(bytes)
+            var content string
+            err := fuzzConsumer.CreateSlice(&content)
+            if err != nil {
+                return 0
+            }
+
             core.Del(content)
             return 0
 
         case 3:
-            content := string(bytes)
+            fuzzConsumer := fuzz.NewConsumer(bytes)
+            var content string
+            err := fuzzConsumer.CreateSlice(&content)
+            if err != nil {
+                return 0
+            }
+
             var pointer = unsafe.Pointer(&content)
             core.DelByPtr(pointer)
             return 0
 
         case 4:
-            content := string(bytes)
+            fuzzConsumer := fuzz.NewConsumer(bytes)
+            var content string
+            err := fuzzConsumer.CreateSlice(&content)
+            if err != nil {
+                return 0
+            }
+            
             var test core.StackRef
             test.Push(content)
             return 0
